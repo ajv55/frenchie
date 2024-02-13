d5d82f5cd9f47a4797da42800845c7cd7d631310
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-02-13 18:15:22 -0500

    trying again

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div style={{
       backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://nypost.com/wp-content/uploads/sites/2/2023/03/NYPICHPDPICT000008292508-1.jpg")'
-    }} className='bg-no-repeat bg-cover w-full h-screen'>
+    }} className='bg-no-repeat bg-cover border w-full h-screen'>
       <div className=" w-full h-screen flex justify-center items-center flex-col ">
           <h1 className={`${dancingScript.className} text-white text-8xl p-5`}>Frenchies bring joy to the world</h1>
           <h3 className="text-white text-5xl p-4">Exotic french bulldogs for sale</h3>
